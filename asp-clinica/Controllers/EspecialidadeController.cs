54d49c7434c3f8858cb6486cf1ecf913c97801ad
valen <valenpschool@gmail.com>
2022-04-28 20:37:44 -0300

    Parte de especialidades do sistema.

---
+++ asp-clinica/Controllers/EspecialidadeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace asp_clinica.Controllers
+{
+    public class EspecialidadeController : Controller
+    {
+        // GET: Especialidade
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
